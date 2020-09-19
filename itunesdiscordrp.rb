5fec7b06787519c8a3003163f6ebfff55575bf91
Kevin Ludovico Mussi <kevinludovico.mussi@mail.polimi.it>
2020-09-19 10:12:21 +0200

    Added cask formula

---
+++ itunesdiscordrp.rb
@@ -0,0 +1,11 @@
+cask "itunesdiscordrp" do
+  version "1.5.6"
+  sha256 "a7d59205d47d84037142f99cc3d35a9bc49cab1357a81bca5a70aa3cfcf9c6fb"
+
+  url "https://github.com/kkevinm/iTunes-Discord-RP/releases/download/1.5.6/iTunesDiscordRP-1.5.6.dmg"
+  name "iTunes Discord RP"
+  desc "A simple app that shows what you're currently listening to in your Discord status."
+  homepage "https://github.com/kkevinm/iTunes-Discord-RP"
+
+  app "iTunesDiscordRP.app"
+end
